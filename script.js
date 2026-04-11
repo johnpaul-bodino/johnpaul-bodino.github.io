31fc108597dbbe0621bfa35eaba35cbb4fe3be16
Johnpaulbodino <johnpaulbodino49@gmail.com>
2026-04-12 03:51:46 +0800

    fixing

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function toggleMenu() {
-    const menu = document.querySelector('.menu-links');
-    const icon = document.querySelector('.hamburger-icon');
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
-    
-}
+(() => {
+  const toggle = document.querySelector(".theme-toggle");
+  if (!toggle) return;
+
+  toggle.addEventListener("click", () => {
+    document.body.classList.toggle("is-light");
+  });
+})();
