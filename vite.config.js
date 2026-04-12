9b32d4dcf9d8288b182cf6da9989badf36135496
Johnpaulbodino <johnpaulbodino49@gmail.com>
2026-04-12 16:33:06 +0800

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/johnpaul-bodino.github.io/", // ✅ FIXED
+  base: "/johnpaul-bodino.github.io/",
 })
