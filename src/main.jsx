5e490617821e6fb6d92197d9110a663a64214343
Johnpaulbodino <johnpaulbodino49@gmail.com>
2026-04-12 15:59:15 +0800

    checking

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './styles/style.css'
+import './styles/mediaqueries.css'
+import './styles/about.css'
+import './styles/project.css'
+import './styles/contact.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
