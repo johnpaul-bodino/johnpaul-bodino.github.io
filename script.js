c957d50f6f737e55085971677b5c183d1ab7af43
Johnpaulbodino <johnpaulbodino49@gmail.com>
2026-04-12 16:23:08 +0800

    HUHUHU

--- script.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  const toggle = document.querySelector(".theme-toggle");
-  if (!toggle) return;
-
-  toggle.addEventListener("click", () => {
-    document.body.classList.toggle("is-light");
-  });
-})();
