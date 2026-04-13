332fb31d60bb4796ea8ab1e6218c94d2485979f9
Johnpaulbodino <johnpaulbodino49@gmail.com>
2026-04-13 10:32:33 +0800

    push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,23 @@ import '../styles/Navbar.css'
 
 export default function Navbar() {
   return (
+    
     <nav className="navbar">
-      <a href="#home" className="navbar-link">Home</a>
-      <a href="#about" className="navbar-link">About</a>
-      <a href="#projects" className="navbar-link">Projects</a>
-      <a href="#skills" className="navbar-link">Skills</a>
-      <a href="#contact" className="navbar-link">Contact</a>
-      <span className="navbar-sun">☀️</span>
+      <img
+          src="https://res.cloudinary.com/dazttfchn/image/upload/v1776013650/logo_hero_tjwk0d.svg"
+          alt="JB Logo"
+          className="home__logo"
+        />
+      <div className="navbar-links">
+          <a href="#home" className="navbar-link">Home</a>
+          <a href="#about" className="navbar-link">About</a>
+          <a href="#projects" className="navbar-link">Projects</a>
+          <a href="#skills" className="navbar-link">Skills</a>
+          <a href="#contact" className="navbar-link">Contact</a>
+        </div>
+      <span className="navbar-sun">
+        <img src="/Sun.svg" alt="Sun Icon" />
+      </span>
     </nav>
   )
 }
